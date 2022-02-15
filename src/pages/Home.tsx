3dbcd890925ade37a228b19d9e92d8104da68512
yiminprogram <minwork1016@outlook.com>
2022-02-15 17:12:43 +0800

    add tailwind css

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -22,6 +22,11 @@ const Home = () => {
       <h1>{count}</h1>
       <button onClick={increase}>Increase</button>
       <button onClick={decrease}>Decrease</button>
+      <div className="p-5">
+        <button className="bg-indigo-700 text-white font-bold py-3 px-5 text-3xl rounded hover:bg-indigo-900 active:bg-indigo-700">
+          Tailwind CSS
+        </button>
+      </div>
     </div>
   );
 };
